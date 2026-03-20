2115ee01e8b46c6994032d73ae4223eba843c13c
Davi Felipe <83774224+daviborgesfelipe@users.noreply.github.com>
2026-03-20 00:52:48 +0000

    alterado mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Hello, world!</h1> 
+    return <div style={{textAlign: "center"}}><h1>Pés na areia!</h1><br /><h2>E SURF na veia!!!</h2></div> 
 }
 
 export default Home;
